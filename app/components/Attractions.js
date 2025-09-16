cf8d98ce175571908b4cb8d836266fd13c7855aa
lwin67 <lwin.m67@rsu.ac.th>
2025-09-16 17:44:43 +0700

    Assignment6

--- app/components/Attractions.js
+++ app/components/Attractions.js
@@ -50,7 +50,7 @@ export default function Attractions() {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold text-center mb-10 text-gray-800">
-        Popular Tourist Attractions
+        Lwin Min Thant 6703240
       </h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
